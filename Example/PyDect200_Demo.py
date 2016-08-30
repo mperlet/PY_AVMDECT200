b840c949b2a5341d1196b9da85075f49d7112185
Frank Schmidt <Kalzifer@users.noreply.github.com>
2016-08-30 16:24:22 +0200

    Update PyDect200_Demo.py

--- Example/PyDect200_Demo.py
+++ Example/PyDect200_Demo.py
@@ -17,10 +17,8 @@ except:
     PyDect200 = PyDect200.PyDect200
 
 print(u"Welcome to PyDect200 v%s, the Python AVM-DECT200 API" % PyDect200.__version__)
-fritzbox_pw = getpass.getpass(prompt='Please insert your fritzbox-password: ', stream=None)
-fritzbox_user = getpass.getpass(prompt='Please insert your fritzbox-username: ', stream=None)
-print(u'Thank you, please wait few seconds...')
-f = PyDect200(fritzbox_pw, fritzbox_user)
+f = PyDect200.PyDect200(fritzbox_pw, fritzbox_user, "fritz.pem","https://fritz.box")
+f.get_sid() # SID will be timedout after 1h you have to fetch an new one after timeout
 try:
         info = f.get_info()
         power = f.get_power_all()
@@ -38,7 +36,6 @@ for dev_id in info.keys():
         except:
             print(u"Device Name:         %s" % dev_name.encode('utf-8').decode('utf-8', 'ignore'))
 
-
         print(u"Device State:        %s" % ('ON' if info.get(dev_id) == '1' else 'OFF'))
         dev_power = power.get(dev_id)
         if dev_power.isdigit():
@@ -46,4 +43,5 @@ for dev_id in info.keys():
         print(u"Device Power:        %sW" % dev_power)
         print(u"Device Energy:       %sWh" % f.get_energy_single(dev_id))
         print(u"Device Temperature:  %s degree Celsius  " % (f.get_temperature_single(dev_id)))
-        print(u'')
+        
+f.logout()
